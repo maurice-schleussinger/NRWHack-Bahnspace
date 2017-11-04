c5abed9e6056dc038ce54cc18f754a45e070f6ae
Maurice Schleußinger <Maurice.Schleussinger@uni-duesseldorf.de>
2017-11-04 16:00:24 +0100

    add event based announcements

--- bahn_bot.py
+++ bahn_bot.py
@@ -1,9 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+bahn_bot.py
+
+"""
+
+if __name__ == '__main__':
+    pass
+from pprint import pprint
 from rocketchat_API.rocketchat import RocketChat
 import config
+import time
 
 rocket = RocketChat(
-    'testbot', 'D2PcQ9yCs&MXB8k', server_url='http://10.82.132.65')
-pprint(rocket.me().json())
-pprint(rocket.chat_post_message('good news everyone!',
-                                channel='GENERAL', alias='RE7').json())
-pprint(rocket.channels_history('GENERAL', count=5).json())
+    config.BAHN_BOT_NAME, config.BAHN_BOT_PW, server_url='http://10.82.132.65')
+
+
+class Event(object):
+    """docstring for Event"""
+
+    def __init__(self, e_type, message):
+        super(Event, self).__init__()
+        self.e_type = e_type
+
+        self.message = message
+
+
+def announce_event(event):
+
+    rocket.chat_post_message("{0} {1}".format(config.FLAIRS[event.e_type], event.message),
+                             channel=config.ANNOUNCE_CHANNEL, alias='RE7').json()
+
+
+if __name__ == '__main__':
+    pprint(rocket.channels_list().json())
+    next_stop = Event(0, "Nächster Halt: Solingen Hbf 🚉")
+    tunnel = Event(
+        0, "Wir fahren gleich in einen Tunnel. Nicht erschrecken. 🚇 👻")
+    wait = Event(-1, "Ein Zug vor uns verzögert unsere Weiterfahrt. 👀")
+    announce_event(tunnel)
+    time.sleep(60)
+    announce_event(wait)
+    time.sleep(360)
+    announce_event(next_stop)
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 BAHN_BOT_NAME = "testbot"
-BAHN_BOT_PW = "MXB8k"
+BAHN_BOT_PW = "D2PcQ9yCs&MXB8k"
 BAHN_NAME = "RE7"
+ANNOUNCE_CHANNEL = "GENERAL"
+FLAIRS = {-1: "📛", 0: "ℹ️", 1: "✅"}
