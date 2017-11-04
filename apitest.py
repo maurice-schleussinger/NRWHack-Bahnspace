0d496d91df6daea7c6de87cd87c40337bd36f3df
t7l <tobias.siebenlist@gmail.com>
2017-11-04 16:33:29 +0100

    using config

--- apitest.py
+++ apitest.py
@@ -1,16 +1,16 @@
 import hug
 from rocketchat_API.rocketchat import RocketChat
+import config
 
-rocket = RocketChat('quentin.quizzer', 'nrwhack', server_url='http://10.82.132.65')
+rocket = RocketChat(config.BAHN_BOT_NAME, config.BAHN_BOT_PW, server_url='http://10.82.132.65')
 
 def channel_messages(rocket, channel_name):
-    json_result = rocket.channels_history('GENERAL').json()
+    json_result = rocket.channels_history(channel_name).json()
     return json_result
 
 def active_users(channel_name):
     cm = channel_messages(rocket, channel_name)['messages']
     users = [x["u"] for x in cm]
-    print(users)
     usernames = set([item["username"] for item in users])
     users_information = {"count":len(usernames), "usernames":usernames, "activity":"high"}
     return users_information
